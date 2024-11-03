c300cb110ab4b8a50ddb0b5428b01b2f3f94180e
mnuka <madhureddy.nuka@gmail.com>
2024-11-03 13:51:49 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,7 @@
-# main.tf
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
-  }
+
+resource "aws_instance" "basics" {
+    ami = var.ami
+    instance_type = var.instance_type
+    provider = aws.us-east-1
+
 }
-
-provider "docker" {}
-
-# Pull the Jenkins Docker image
-resource "docker_image" "jenkins" {
-  name = "jenkins/jenkins:lts"
-  keep_locally = false
-}
-
-# Create and run the Jenkins container
-resource "docker_container" "jenkins_container" {
-  name  = "jenkins_container"
-  image = docker_image.jenkins.name
-
-  # Map ports for Jenkins Web UI and agent connections
-  ports {
-    internal = 8080
-    external = 8080
-  }
-
-  ports {
-    internal = 50000
-    external = 50000
-  }
-}
-
